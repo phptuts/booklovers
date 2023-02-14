10ac83118d56ef15b9f654f15aade6a0bec2e1d9
phptuts <glaserpower@gmail.com>
2023-02-13 22:24:08 -0800

    6-messages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
 <script>
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import Nav from '$lib/components/Nav.svelte';
+	import messagesStore from '$lib/stores/messages.store';
+
+	function closeMessage() {
+		messagesStore.hide();
+	}
 </script>
 
 <Nav />
 <main class="container">
+	{#if $messagesStore.show}
+		<div class="row mt-3">
+			<div class="col">
+				<div
+					class:alert-danger={$messagesStore.type === 'error'}
+					class:alert-success={$messagesStore.type === 'success'}
+					class="alert alert-dismissible"
+					role="alert"
+				>
+					<strong>{$messagesStore.type === 'error' ? 'Error' : 'Success'}:</strong>
+					{$messagesStore.message}
+					<button type="button" on:click={closeMessage} class="btn-close" aria-label="Close" />
+				</div>
+			</div>
+		</div>
+	{/if}
 	<slot />
 </main>
