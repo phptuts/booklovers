5791fde7b13e90c9d0fecb113cd48107de8c14fd
phptuts <glaserpower@gmail.com>
2023-02-23 23:26:53 -0800

    27-enhanced-forms

--- src/lib/components/Books/BookForm.svelte
+++ src/lib/components/Books/BookForm.svelte
@@ -1,14 +1,25 @@
 <script>
+	import { enhance } from '$app/forms';
+
 	export let form;
+	let submitting = false;
+
+	$: if (form && form.success === false) {
+		submitting = false;
+	}
+
+	function submitForm(e) {
+		submitting = true;
+	}
 </script>
 
-<form enctype="multipart/form-data" method="POST">
+<form on:submit={submitForm} use:enhance enctype="multipart/form-data" method="POST">
 	<div class="mb-3">
 		<label for="title" class="form-label">Book Title</label>
 		<input
 			type="text"
 			name="title"
-			class="form-control is-invalid"
+			class="form-control"
 			id="title"
 			value={form?.title || ''}
 			class:is-invalid={form?.error_title}
@@ -91,5 +102,11 @@
 			<div class="invalid-feedback">{form?.error_small_picture}</div>
 		{/if}
 	</div>
-	<button type="submit" class="btn btn-primary w-100"> Submit </button>
+	<button disabled={submitting} type="submit" class="btn btn-primary w-100">
+		{#if submitting}
+			Submitting...
+		{:else}
+			Submit
+		{/if}
+	</button>
 </form>
