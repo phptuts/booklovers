46ea6eb296e7e5b1acfedfed807d6a1b527dcf0f
phptuts <glaserpower@gmail.com>
2023-02-24 22:50:25 -0800

    34-home-page-books-component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { goto } from '$app/navigation';
 	import messagesStore from '$lib/stores/messages.store';
+	import Book from '$lib/components/Books/Book.svelte';
 
 	export let data;
 
@@ -21,13 +22,18 @@
 	}
 </script>
 
-<ul>
-	{#each data.books as book (book.id)}
-		<li>{book.title}</li>
-	{/each}
-</ul>
-
 <div class="row">
+	<div class="col">
+		<h1>Latest Books</h1>
+		<h3>Page: {data.page}</h3>
+	</div>
+</div>
+
+{#each data.books as book (book.id)}
+	<Book {book} />
+{/each}
+
+<div class="row mt-3">
 	<div class="col">
 		<button on:click={back} disabled={!data.previous} class="btn btn-info w-100">Previous</button>
 	</div>
