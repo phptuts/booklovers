1222cba1250445e36a5b47c281015c82cbeab8ae
phptuts <glaserpower@gmail.com>
2023-02-25 14:04:51 -0800

    41-alerts-new-books

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,31 @@
 	import { onMount } from 'svelte';
 	import { sendJWTToken } from '$lib/firebase/auth.client';
 	import authStore from '$lib/stores/auth.store';
+	import bookNotifyStore from '$lib/stores/book-notify.store';
+	import { onDestroy } from 'svelte';
+
+	let notifyBook;
+
+	const unsub = bookNotifyStore.subscribe((book) => {
+		console.log(notifyBook);
+		if (!$authStore.isLoggedIn) {
+			notifyBook = book;
+			return;
+		}
+
+		if ($authStore.userId !== book.user_id) {
+			notifyBook = book;
+			return;
+		}
+	});
+
+	onDestroy(() => {
+		unsub();
+	});
+
+	function closeAlert() {
+		notifyBook = null;
+	}
 
 	export let data;
 
@@ -64,4 +89,28 @@
 		</div>
 	{/if}
 	<slot />
+	{#if notifyBook}
+		<div
+			class="toast show position-fixed top-0 end-0 m-3"
+			role="alert"
+			aria-live="assertive"
+			aria-atomic="true"
+		>
+			<div class="toast-header">
+				<strong class="me-auto">New Book</strong>
+				<button
+					on:click={closeAlert}
+					type="button"
+					class="btn-close"
+					data-bs-dismiss="toast"
+					aria-label="Close"
+				/>
+			</div>
+			<div class="toast-body">
+				Book <a data-sveltekit-preload-data="hover" href="/book/{notifyBook.id}"
+					>{notifyBook.title}</a
+				> just created!!
+			</div>
+		</div>
+	{/if}
 </main>
